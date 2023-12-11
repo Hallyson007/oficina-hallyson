1a4e312398457249b797f4e09695e55eb73cca7b
hallyson <hallysonmartins@mail.com>
2023-12-11 19:55:48 -0300

    ola

--- processar.php
+++ processar.php
@@ -11,46 +11,37 @@
     <?php
     
     $pneu = $_POST['pneu'];
-    
     $oleo = $_POST['oleo'];
-    
     $vela = $_POST['vela'];
-    
     $valortotal = 0.00;
-    
     $qtdtotal = 0;
     
     define ('PRECOPNEU', 100);
-
     define ('PRECOOLEO', 10);
-
     define ('PRECOVELA', 4);
-
     $valortotal = ($pneu*PRECOPNEU) + ($oleo * PRECOOLEO) + ($vela*PRECOVELA); 
-
     echo '<p> Pedido Processado ás :'.date('H:i - d/m/y').'</p>';
-    
-   
-    
     $qtdtotal = $pneu + $oleo + $vela;
-    if ($qtdtotal ==0) {
-    
+    if($qtdtotal ==0) {
         echo 'você nao pediu nada. <br>' ;
-    } else { echo $pneu .' pneus <br>';
-    
-        echo $oleo .' galões <br>';
-        
-        echo $vela .' velas <br>';
+    }
+     else {    echo $pneu .' pneus <br>';
+                echo $oleo .' galões <br>';
+                echo $vela .' velas <br>';
         }
-        
+
         echo 'A quantidade total é :'.$qtdtotal.'<br>';
     
     echo 'O valor total é R$: '.number_format($valortotal,2,",",".");
     $taxa = 0.22 ; // lucru presumido
     $valortotal = $valortotal + (1+ $taxa);
      echo 'O valor total é R$: '.number_format($valortotal,2,",",".");   
- ?>
  
+ 
+ ?>
+
+ 
+
     <p><a  href="index.php">Voltar</a></p>
 
 
